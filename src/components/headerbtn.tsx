d8505a0fe1c827bcd33a8c4cdea7f068e74e02be
aerin <92151406+aerinxo@users.noreply.github.com>
2025-01-12 19:00:55 +0530

    more chngs

--- src/components/headerbtn.tsx
+++ src/components/headerbtn.tsx
@@ -4,7 +4,7 @@ export function ShimmerButtonDemo3() {
   return (
     <div className="z-10 flex min-h-8 items-center justify-center">
       <ShimmerButton className="shadow-2xl">
-        <span className="whitespace-pre-wrap text-center text-xs font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+        <span className="whitespace-pre-wrap text-center text-base font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-base">
           Contact
         </span>
       </ShimmerButton>
