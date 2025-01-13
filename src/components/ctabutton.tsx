2251c6865eb67c50efc6256af72ac3e27c223b27
Maximus <hey@m4xy.org>
2025-01-13 21:26:28 +0530

    CTA

--- src/components/ctabutton.tsx
+++ src/components/ctabutton.tsx
@@ -4,8 +4,12 @@ import ShimmerButton from "./magicui/button";
 export function ShimmerButtonDemo() {
   return (
     <div className="z-10 flex min-h-16 items-center justify-center">
-      <Link href="/contact">
-      <ShimmerButton className="shadow-2xl">
+      <Link
+        href="https://www.ieee.org/membership-application/public/join.html?grade=Student"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <ShimmerButton className="shadow-2xl">
         <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
           Join today
         </span>
