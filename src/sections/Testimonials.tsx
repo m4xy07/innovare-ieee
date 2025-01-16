a49ae31615894e140b937af21b3e3904fae86e08
Maximus <hey@m4xy.org>
2025-01-16 21:45:28 +0530

    HArsh

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,7 +7,7 @@ import avatar5 from "@/assets/avatar-5.png";
 import avatar6 from "@/assets/avatar-6.png";
 import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
-import avatar9 from "@/assets/avatar-9.png";
+import avatar9 from "@/assets/avatar-10.png";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 import { motion } from "framer-motion";
@@ -66,8 +66,8 @@ const testimonials = [
   {
     text: "Quantitative research work has never been easier due the Chapter and organisation's vast reach.",
     imageSrc: avatar9.src,
-    name: "Aneeqa Inamdar",
-    username: "aneeqa.inamdar@innovare.dev",
+    name: "Harsh Chavan",
+    username: "harsh.chavan@innovare.dev",
   },
 ];
 
@@ -94,30 +94,31 @@ const TestimonialsColumn = (props: {
       className="flex flex-col gap-6 pb-6"
     >
       {[...new Array(2)].fill(0).map((_, outerIndex) => (
-  <React.Fragment key={`outer-${outerIndex}`}>
-    {props.testimonials.map(({ text, imageSrc, name, username }, innerIndex) => (
-      <div className="card" key={`inner-${outerIndex}-${innerIndex}`}>
-        <div>{text}</div>
-        <div className="flex items-center gap-2 mt-5">
-          <Image
-            src={imageSrc}
-            alt={name}
-            width={40}
-            height={40}
-            className="h-10 w-10 rounded-full"
-          />
-          <div className="flex flex-col">
-            <div className="font-medium tracking-tight leading-5">
-              {name}
-            </div>
-            <div className="leading-5 tracking-tight">{username}</div>
-          </div>
-        </div>
-      </div>
-    ))}
-  </React.Fragment>
-))}
-
+        <React.Fragment key={`outer-${outerIndex}`}>
+          {props.testimonials.map(
+            ({ text, imageSrc, name, username }, innerIndex) => (
+              <div className="card" key={`inner-${outerIndex}-${innerIndex}`}>
+                <div>{text}</div>
+                <div className="flex items-center gap-2 mt-5">
+                  <Image
+                    src={imageSrc}
+                    alt={name}
+                    width={40}
+                    height={40}
+                    className="h-10 w-10 rounded-full"
+                  />
+                  <div className="flex flex-col">
+                    <div className="font-medium tracking-tight leading-5">
+                      {name}
+                    </div>
+                    <div className="leading-5 tracking-tight">{username}</div>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </React.Fragment>
+      ))}
     </motion.div>
   </div>
 );
@@ -130,14 +131,17 @@ export const Testimonials = () => {
           <div className="flex justify-center">
             <div className="tag text-white">Testimonials</div>
           </div>
-          <h2 className="section-title mt-5 text-white">What our members say</h2>
+          <h2 className="section-title mt-5 text-white">
+            What our members say
+          </h2>
           <p className="section-description mt-5 text-white text-opacity-30 pb-10">
-            Hear from our members: Inspiring journeys, impactful experiences, and a community that drives innovation.
+            Hear from our members: Inspiring journeys, impactful experiences,
+            and a community that drives innovation.
           </p>
         </div>
-        
+
         <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[738px] overflow-hidden">
-        <DotPatternDemo />
+          <DotPatternDemo />
           <TestimonialsColumn testimonials={firstColumn} duration={15} />
           <TestimonialsColumn
             testimonials={secondColumn}
